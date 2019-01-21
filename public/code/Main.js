b214ec5bd3a193f8127c0c058e8648ff42982707
Flizzet <pedro@flizzet.com>
2019-01-20 18:10:44 -0800

    Added download button functionality

--- public/code/Main.js
+++ public/code/Main.js
@@ -20,6 +20,21 @@ require([
                 GameManager.GAME.initGame();
             });
 
+        // Set up download button
+        $('.download-button').click(function(e) {
+            console.log("Download button clicked");
+            // Open download link based on respective OS
+            switch (getMobileOperatingSystem()) {
+                case "iOS":
+                    window.location.href = "https://itunes.apple.com/us/app/via-low-cost-ride-sharing/id657777015?mt=8";
+                break;
+                case "Android": // Fall through intended
+                default:
+                    window.location.href = "https://play.google.com/store/apps/details?id=via.rider&hl=en_US";
+                break;
+            }
+        });
+
         // Set up text code button
         $('.text-code-link').click(function(e) {
             $('.prompt-container .text-phone-form').fadeIn(300);
@@ -73,6 +88,24 @@ require([
         $('.text-code-link').fadeOut(300);
         $('.prompt-container .text-phone-form').fadeOut(300);
     }
+
+    function getMobileOperatingSystem() {
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      
+        if (/windows phone/i.test(userAgent)) {
+            return "Windows Phone";
+        }
+    
+        if (/android/i.test(userAgent)) {
+            return "Android";
+        }
+    
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            return "iOS";
+        }
+      
+          return "unknown";
+      }
  
     // Start game
     StartGame();

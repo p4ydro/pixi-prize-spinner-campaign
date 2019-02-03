e72d3c22ff3726906f26073a9f67e7aa4186cfd4
Flizzet <pedrodutra10@gmail.com>
2019-02-03 10:45:08 -0800

    Fixed issue causing share buttons to not appear in Safari iOS

--- public/code/InviteFriends.js
+++ public/code/InviteFriends.js
@@ -1,11 +1,34 @@
-$('.share-button-clickable').click(function(e) {
-    // Use a post to enter thank you page, ensures internet connection
-    $.post("/invitefriends", { intent: "share", type: this.dataset.type }, function(data) {
-        if (data == "OK") {
-            enterThankYouPage();
-        }
-    });
-});
+// $('.share-button-clickable').click(function(e) {
+//     // Use a post to enter thank you page, ensures internet connection
+//     $.post("/invitefriends", { intent: "share", type: this.dataset.type }, function(data) {
+//         if (data == "OK") {
+//             enterThankYouPage();
+//         }
+//     });
+// });
+
+function shareButtonClick(intent, inviteLink) {
+
+    console.log("Share button clicked with intent: " + intent + ", and invite link of: " + inviteLink);
+
+    var smsLink = "sms:?&body=I%20ride%20with%20Via%2C%20you%20should%20too!%20Spin%20the%20wheel%20to%20score%20a%20special%20prize.%20" + inviteLink + "";
+    var emailLink = "mailto:?to=&body=%20I%20ride%20with%20Via%2C%20you%20should%20too!%20Rides%20are%20fast%2C%20comfy%2C%20and%20always%20a%20low%20fare.%20Follow%20this%20link%20to%20spin%20the%20prize%20wheel%20and%20score%20a%20special%20sign%20up%20bonus.%20" + inviteLink + "&subject=Spin%20the%20Via%20prize%20wheel";
+    var facebookLink = "https://www.facebook.com/sharer/sharer.php?u=https://ridewithvia.com/&quote=I%20ride%20with%20Via%2C%20you%20should%20too!%20Rides%20are%20fast%2C%20comfy%2C%20and%20always%20a%20low%20fare.%20Follow%20this%20link%20to%20spin%20the%20prize%20wheel%20and%20score%20a%20special%20sign%20up%20bonus.%20" + inviteLink + "";
+
+    switch (intent) {
+        case 'SMS':
+            window.open(smsLink);
+        break;
+        case 'Email':
+            window.open(emailLink);
+        break;
+        case 'Facebook':
+            window.open(facebookLink);
+        break;
+    }
+
+    enterThankYouPage();
+}
 
 function showHowItWorksPrompt() {
     $('.darkener').addClass('shown');

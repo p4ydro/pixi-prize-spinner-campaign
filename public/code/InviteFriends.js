0f69c692d7d26530609f7dd2b6c17e8580dc1989
Flizzet <pedrodutra10@gmail.com>
2019-02-03 11:09:46 -0800

    Update InviteFriends.js

--- public/code/InviteFriends.js
+++ public/code/InviteFriends.js
@@ -5,9 +5,6 @@ $(document).ready(function() {
 });
 
 function shareButtonClick(intent, inviteLink) {
-
-    console.log("Share button clicked with intent: " + intent + ", and invite link of: " + inviteLink);
-
     var smsLink = "sms:?&body=I%20ride%20with%20Via%2C%20you%20should%20too!%20Spin%20the%20wheel%20to%20score%20a%20special%20prize.%20" + inviteLink + "";
     var emailLink = "mailto:?to=&body=%20I%20ride%20with%20Via%2C%20you%20should%20too!%20Rides%20are%20fast%2C%20comfy%2C%20and%20always%20a%20low%20fare.%20Follow%20this%20link%20to%20spin%20the%20prize%20wheel%20and%20score%20a%20special%20sign%20up%20bonus.%20" + inviteLink + "&subject=Spin%20the%20Via%20prize%20wheel";
     var facebookLink = "https://www.facebook.com/sharer/sharer.php?u=https://ridewithvia.com/&quote=I%20ride%20with%20Via%2C%20you%20should%20too!%20Rides%20are%20fast%2C%20comfy%2C%20and%20always%20a%20low%20fare.%20Follow%20this%20link%20to%20spin%20the%20prize%20wheel%20and%20score%20a%20special%20sign%20up%20bonus.%20" + inviteLink + "";

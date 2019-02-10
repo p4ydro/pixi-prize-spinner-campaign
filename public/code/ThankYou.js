d6cd954237318bd02b6e040e473a7cfa62c68c8b
Pedro Dutra <pedro@flizzet.com>
2019-02-10 05:15:55 -0800

    Fixed spinning text appearing below screen on Safari iOS iPhone 7

---
+++ public/code/ThankYou.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    positionTermsContainer();
+});
+
+$(window).resize(function() {
+    positionTermsContainer();
+});
+
+function positionTermsContainer() {
+    var tc = $('.tc-link-container');
+    tc.offset({ top: (window.innerHeight - tc.height() - 40) });
+}

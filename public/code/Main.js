5e290b448ddb78b51b316cfe76543c328b79fefb
Pedro Dutra <pedro@flizzet.com>
2019-02-11 04:11:18 -0800

    Fixed centering of Prize Prompt in game page on iOS WebKit based browsers

--- public/code/Main.js
+++ public/code/Main.js
@@ -21,6 +21,18 @@ require([
                 GameManager.GAME.initGame();
             });
 
+        // Prize prompt centering for iOS WebKit based browsers
+        var ua = window.navigator.userAgent;
+        var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+        var webkit = !!ua.match(/WebKit/i);
+        var iOSWebKit = iOS && webkit;
+        if (iOSWebKit) {
+            var clientHeight = document.documentElement.clientHeight;
+            $('.prompt-container').css({
+                "top": (clientHeight / 2)
+            });
+        }
+
         // Set up download button
         $('.download-button').click(function(e) {
             console.log("Download button clicked");

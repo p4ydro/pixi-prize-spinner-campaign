21773d4eb9681f2f4b23e91956483e35ecd630bf
Pedro Dutra <pedro@flizzet.com>
2019-02-17 02:15:03 -0800

    Fixed R&C overlap in ThankYou page

--- public/code/ThankYou.js
+++ public/code/ThankYou.js
@@ -10,4 +10,21 @@ function positionTermsContainer() {
     var tc = $('.tc-link-container');
     tc.offset({ top: (window.innerHeight - tc.height() - 40) });
     tc.css('opacity', '1');
+
+    // Check for overlap with paragraph
+    var tcTop = tc.offset().top;
+    var dft = $(".dont-forget-text");
+    var dftBottom = dft.offset().top + dft.height();
+
+    $('.text-container h1').html('Y: ' + window.innerHeight);
+
+    if (window.innerHeight < 500) {
+        console.log("Overlap");
+        // Position between DFT and bottom
+        var dftBottom = dft.offset().top + dft.height();
+        var newTop = dftBottom + ((window.innerHeight - dftBottom) / 2);
+
+        tc.offset({ top: newTop });
+
+    }
 }
